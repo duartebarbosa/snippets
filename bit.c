57b9569372661d5149af44f4e44af6ff5343184b
Duarte Barbosa <duarte.barbosa@gmail.com>
2013-06-08 18:01:41 +0100

    added itobs

--- bit.c
+++ bit.c
@@ -1,2 +1,20 @@
-#define HINIBBLE(b) (((b) >> 4) & 0x0F)
-#define LONIBBLE(b) ((b) & 0x0F)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define HIGH_NIBBLE(b) (((b) >> 4) & 0x0F)
+#define LOW_NIBBLE(b) ((b) & 0x0F)
+
+
+//int to binary string, x86 int beware.
+//returning string must be freed!
+char* itobs(int value){
+	int j = 31;
+	char aux[33] = {0};
+
+	for(; value; value >>= 1)
+		aux[j--] = value % 2 ? '1' : '0';
+
+	return strcpy(calloc(32-j, 1), aux + j + 1);
+}
+
