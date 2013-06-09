dce94c792ae869a05863772cb190e67e5c908629
Duarte Barbosa <duarte.barbosa@gmail.com>
2013-06-09 01:55:13 +0100

    added kSparseIntegers

--- bit.c
+++ bit.c
@@ -18,6 +18,29 @@ char* itobs(int value){
 	return strcpy(calloc(32-j, 1), aux + j + 1);
 }
 
+// A positive integer N is called K-sparse if there are at least K 0s between any two consecutive 1s in its binary representation.
+int kSparseIntegers(int value, int k){
+	int i = 0, tmp = 0;
+	char *aux = itobs(value);
+	
+	for(; aux[i] != '\0'; i++){
+		if(aux[i] == '0')
+			tmp++;
+		else{
+			if(i == 0 || aux[i+1] == '\0')
+				continue;
+			else if(tmp >= k)
+				tmp = 0;
+			else
+				return -1;
+		}
+	}
+	
+	return 0;
+			
+}
+
+//maximal consecutive zeros in the binary representation of a number
 int consecutiveZeros(char *value){
 	int index = 0, tmp = 0, counter = 0;
 	
